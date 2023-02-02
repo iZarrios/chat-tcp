2d5be4553afedf2a09466314c50978d58c42cbaa
iZarrios <eyad_zarrios2001@yahoo.com>
2023-02-02 18:37:09 +0200

    Add cmd enum

--- tcpchat/cmd.go
+++ tcpchat/cmd.go
@@ -1,6 +1,17 @@
 package tcpchat
 
+//https://www.tutorialspoint.com/how-to-use-iota-in-golang
+
+const (
+	CMD_NICK CmdID = iota
+	CMD_JOIN
+	CMD_ROOMS
+	CMD_MSG
+	CMD_QUIT
+)
+
 type CmdID int
+
 type Cmd struct {
 	ID     CmdID
 	Client *Client
