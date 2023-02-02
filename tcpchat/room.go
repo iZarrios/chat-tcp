682a77eead19d6578dda8922ecfbb0497d850b89
iZarrios <eyad_zarrios2001@yahoo.com>
2023-02-02 20:48:57 +0200

    Add broadcast to room

--- tcpchat/room.go
+++ tcpchat/room.go
@@ -6,3 +6,11 @@ type Room struct {
 	Name    string
 	Members map[net.Addr]*Client
 }
+
+func (r *Room) Broadcast(sender *Client, msg string) {
+	for addr, m := range r.Members {
+		if sender.Conn.RemoteAddr() != addr {
+			m.SendMsgToClient(msg + "\n")
+		}
+	}
+}
