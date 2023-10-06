1fd768cbdc7589cd5065b3b7795b13b383c6619a
iZarrios <zarriose@gmail.com>
2023-10-06 16:54:36 +0300

    fmt

--- tcpchat/tcpchat.go
+++ tcpchat/tcpchat.go
@@ -7,12 +7,6 @@ import (
 func Tcpchat() {
 	s := NewServer(":3000")
 
-	// go func() {
-	// 	for e := range s.msgChannel {
-	// 		fmt.Print(string(e))
-	// 	}
-	// }()
-
 	err := s.Start()
 	if err != nil {
 		log.Fatal("Could not start server ", err)
